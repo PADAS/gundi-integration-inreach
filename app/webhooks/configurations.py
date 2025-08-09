13e8b2b416d03e5818438591db996df3bfd6b80d
Mariano Martinez Grasso <marianomartinez84@gmail.com>
2025-08-09 09:01:48 -0300

    Improvements in webhook configs for inreach

--- app/webhooks/configurations.py
+++ app/webhooks/configurations.py
@@ -1,12 +1,19 @@
-import pydantic
-from .core import WebhookPayload, GenericJsonTransformConfig
+from .core import WebhookPayload, WebhookConfiguration
 from .inreach import InreachEventPayload
+from app.services.utils import UISchemaModelMixin, GlobalUISchemaOptions
 
 
 class InReachWebhookPayload(InreachEventPayload, WebhookPayload):
     pass
 
 
-class InReachWebhookConfig(GenericJsonTransformConfig):
+class InReachWebhookConfig(WebhookConfiguration):
     include_observations: bool = True
     include_messages: bool = True
+
+    ui_global_options: GlobalUISchemaOptions = GlobalUISchemaOptions(
+        order=[
+            "include_messages",
+            "include_observations",
+        ],
+    )
