05284411c7c96906ca10231400b94bde98852844
Mariano Martinez Grasso <marianomartinez84@gmail.com>
2025-08-14 10:34:53 -0300

    Remove global setting for inreach api url

--- app/settings/integration.py
+++ app/settings/integration.py
@@ -2,5 +2,4 @@ from environs import Env
 
 env = Env()
 
-INREACH_API_URL = env.str("INREACH_API_URL", "https://eur-enterprise.inreach.garmin.com")
 TRACING_ENABLED = env.bool("TRACING_ENABLED", True)
