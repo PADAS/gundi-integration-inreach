e1db17141ecc6829678c4ac17914da6427732db6
Mariano Martinez Grasso <marianomartinez84@gmail.com>
2025-08-14 17:32:42 -0300

    Make loading tracing context more resilient

--- app/actions/tracing/pubsub_instrumentation.py
+++ app/actions/tracing/pubsub_instrumentation.py
@@ -3,7 +3,8 @@ import json
 from opentelemetry import propagate, context
 
 
-def load_context_from_attributes(attributes):
+def load_context_from_attributes(attributes=None):
+    attributes = attributes or {}
     carrier = json.loads(attributes.get("tracing_context", "{}"))
     ctx = propagate.extract(carrier=carrier)
     context.attach(ctx)
